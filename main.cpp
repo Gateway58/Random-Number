bc9ea1174824d2239f1669058bf6248265df6f8a
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-05-10 11:46:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 using namespace std; 
+void Max(int bill[10]);
+void Min(int wurtz[10]); 
 int count1 = 0; 
 int count2 = 0; 
 int count3 = 0; 
@@ -25,8 +27,9 @@ int count10 = 0;
 int i = 0; 
 int main()
 {
+ 
    srand(time(NULL)); 
-    for (i; i < 1000000; i++)
+    for (i = 0; i < 100000000; i++)
     {
        int n = rand() %10 +1; 
     switch (n) {
@@ -63,9 +66,124 @@ int main()
     std::cout << "" <<std::endl; 
     cout << "10: "<< count10 << endl; 
     std::cout << "" <<std::endl; 
-    cout << i << endl; 
+    
     int total = count1 + count2 + count3 +count4+count5+count6+count7+count8+count9+count10; 
+   int billwurtz[10] = {count1,count2 , count3 ,count4,count5,count6,count7,count8,count9,count10 }; 
+   Max(billwurtz); 
+   Min(billwurtz); 
+   cout << i << endl; 
     cout <<total <<endl; 
-
+      
     return 0; 
+}
+
+void Max(int bill[10]) 
+{
+   int temp = max(bill[0], bill[1]); 
+   int temp1 = max(temp, bill[3]); 
+   int temp2 = max(temp1, bill[4]); 
+   int temp3 = max(temp2, bill[5]); 
+   int temp4 = max(temp3, bill[6]); 
+   int temp5 = max(temp4, bill[7]); 
+   int temp6 = max(temp5, bill[8]); 
+   int fin = max(temp6, bill[9]); 
+   if (fin == bill[0])
+   {
+     cout << "Highest was 1 with " << fin << endl; 
+   }
+   if (fin == bill[1])
+   {
+     cout << "Highest was 2 with " << fin << endl; 
+   }
+   if (fin == bill[2])
+   {
+     cout << "Highest was 3 with " << fin << endl; 
+   }
+   if (fin == bill[3])
+   {
+     cout << "Highest was 4 with " << fin << endl; 
+   }
+   if (fin == bill[4])
+   {
+     cout << "Highest was 5 with " << fin << endl; 
+   }
+   if (fin == bill[5])
+   {
+     cout << "Highest was 6 with " << fin << endl; 
+   }
+   if (fin == bill[6])
+   {
+     cout << "Highest was 7 with " << fin << endl; 
+   }
+   if (fin == bill[7])
+   {
+     cout << "Highest was 8 with " << fin << endl; 
+   }
+   if (fin == bill[8])
+   {
+     cout << "Highest was 9 with " << fin << endl; 
+   }
+   if (fin == bill[9])
+   {
+     cout << "Highest was 10 with " << fin << endl; 
+   }
+
+   
+
+   
+   
+   
+   
+}
+void Min(int wurtz[10])
+{
+   int temp = min(wurtz[0], wurtz[1]); 
+   int temp1 = min(temp, wurtz[3]); 
+   int temp2 = min(temp1, wurtz[4]); 
+   int temp3 = min(temp2, wurtz[5]); 
+   int temp4 = min(temp3, wurtz[6]); 
+   int temp5 = min(temp4, wurtz[7]); 
+   int temp6 = min(temp5, wurtz[8]); 
+   int fin = min(temp6, wurtz[9]); 
+   if (fin == wurtz[0])
+   {
+     cout << "Lowest was 1 with " << fin << endl; 
+   }
+   if (fin == wurtz[1])
+   {
+     cout << "Lowest was 2 with " << fin << endl; 
+   }
+   if (fin == wurtz[2])
+   {
+     cout << "Lowest was 3 with " << fin << endl; 
+   }
+   if (fin == wurtz[3])
+   {
+     cout << "Lowest was 4 with " << fin << endl; 
+   }
+   if (fin == wurtz[4])
+   {
+     cout << "Lowest was 5 with " << fin << endl; 
+   }
+   if (fin == wurtz[5])
+   {
+     cout << "Lowest was 6 with " << fin << endl; 
+   }
+   if (fin == wurtz[6])
+   {
+     cout << "Lowest was 7 with " << fin << endl; 
+   }
+   if (fin == wurtz[7])
+   {
+     cout << "Lowest was 8 with " << fin << endl; 
+   }
+   if (fin == wurtz[8])
+   {
+     cout << "Lowest was 9 with " << fin << endl; 
+   }
+   if (fin == wurtz[9])
+   {
+     cout << "Lowest was 10 with " << fin << endl; 
+   }
+
 }
